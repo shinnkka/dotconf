e3e17d347f71e590ec92bc7648ada98dc93ac433
c-xk <1762977362@qq.com>
2026-08-05 23:32:32 +0800

    migrate to v5

--- .config/hypr/noctalia/bind.lua
+++ .config/hypr/noctalia/bind.lua
@@ -1,42 +1,30 @@
-local ipc = "qs -c noctalia-shell ipc call"
+local ipc = "noctalia msg"
 local mainMod = "SUPER"
-local run = "uwsm-app -- "
+
 hl.on("hyprland.start", function()
-	hl.exec_cmd(run .. "qs -c noctalia-shell --no-duplicate")
+	hl.exec_cmd("uwsm app -- noctalia")
 end)
 
---hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(ipc .. " launcher toggle"))
+hl.bind(mainMod .. "+Space", hl.dsp.exec_cmd(ipc .. " panel-toggle launcher"))
+hl.bind(mainMod .. " + I", hl.dsp.exec_cmd(ipc .. " panel-toggle control-center"))
 
-hl.bind(mainMod .. " + I", hl.dsp.exec_cmd(ipc .. " controlCenter toggle"))
+hl.bind(mainMod .. "+comma", hl.dsp.exec_cmd(ipc .. " settings-toggle"))
 
-hl.bind(mainMod .. " + comma", hl.dsp.exec_cmd(ipc .. " settings toggle"))
+hl.bind(mainMod .. " + SHIFT + N", hl.dsp.exec_cmd(ipc .. " notification-clear-history"))
 
-hl.bind(mainMod .. " + M", hl.dsp.exec_cmd(ipc .. " sessionMenu toggle"))
+hl.bind("ALT + V", hl.dsp.exec_cmd(ipc .. " panel-toggle clipboard"))
 
-hl.bind(mainMod .. " + N", hl.dsp.exec_cmd(ipc .. " notifications toggleHistory"))
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(ipc .. " volume-up"))
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(ipc .. " volume-down"))
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd(ipc .. " volume-mute"))
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(ipc .. " brightness-up"))
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(ipc .. " brightness-down"))
 
-hl.bind(mainMod .. " + SHIFT + N", hl.dsp.exec_cmd(ipc .. " notifications clear"))
-
---hl.bind("ALT + V", hl.dsp.exec_cmd(ipc .. " launcher clipboard"))
-
-hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd(ipc .. " volume increase"), { locked = true, repeating = true })
-
-hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd(ipc .. " volume decrease"), { locked = true, repeating = true })
-
-hl.bind("XF86AudioMute", hl.dsp.exec_cmd(ipc .. " volume muteOutput"), { locked = true })
-
-hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd(ipc .. " volume muteInput"), { locked = true })
-
-hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd(ipc .. " brightness increase"), { locked = true, repeating = true })
-
-hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd(ipc .. " brightness decrease"), { locked = true, repeating = true })
 hl.layer_rule({
 	name = "noctalia",
-
 	match = {
-		namespace = "noctalia-background-.*$",
+		namespace = "^noctalia-(bar-.+|notification|dock|panel|attached-panel|osd)$",
 	},
-
 	ignore_alpha = 0.5,
 	blur = true,
 	blur_popups = true,
